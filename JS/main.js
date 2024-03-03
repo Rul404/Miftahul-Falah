769d06a6c23cbf7e7068b9b8f364276316a4bbd8
RulGanZz <155933386+Rul404@users.noreply.github.com>
2024-03-03 14:03:19 +0800

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,46 @@
+const x = document.getElementById('menuLink')
+function menuClick() {
+    if (x.style.display === 'none') {
+        x.style.display = 'block'
+    } else {
+        x.style.display = 'none'
+    }
+}
+
+
+const a = document.getElementById("linkDetail")
+function clickDetail() {
+    if (a.style.display === 'none') {
+        a.style.display = 'block'
+    } else {
+        a.style.display = 'none'
+    }
+}
+
+const b = document.getElementById("linkDetaill")
+function clickDetaill() {
+    if (b.style.display === 'none') {
+        b.style.display = 'block'
+    } else {
+        b.style.display = 'none'
+    }
+}
+
+const c = document.getElementById("linkDetailll")
+function clickDetailll() {
+    if (c.style.display === 'none') {
+        c.style.display = 'block'
+    } else {
+        c.style.display = 'none'
+    }
+}
+
+const d = document.getElementById("linkDetaillll")
+function clickDetailll() {
+    if (d.style.display === 'none') {
+        d.style.display = 'block'
+    } else {
+        d.style.display = 'none'
+    }
+}
+
